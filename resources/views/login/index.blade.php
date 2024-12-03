872e5c5f55135634165c678014498a8bd70ef811
Nolan-16 <shinya.watahiki.rensa@gmail.com>
2024-12-03 15:06:31 +0900

    web編終了

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -7,40 +7,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title></title>
+
+    <link rel="stylesheet" href="/css/build/login/index.css">
 </head>
 
 <body>
+    <main id="login">
     <?php if ($variables["isLoginActive"]) { ?>
+        <h2 class="title">ログイン状態</h2>
         <a href="/login/unregister">ログアウト</a>
     <?php } else { ?>
-    <h2>新規登録</h2>
-    <form method="POST" action="/login/register">
+    <h2 class="title">新規登録</h2>
+    <form ref="registerForm" method="POST" action="/login/register">
         @csrf
         <div>
-            ID : <input type="text" name="id">
+            ID : <input type="text" v-model="id" name="id">
         </div>
         <div>
-            PW : <input type="text" name="password">
+            PW : <input type="text" v-model="password" name="password">
         </div>
         <div>
-            <input type="submit" value="送信">
+            <button type="button" v-on:click="registerSubmit">送信</button>
         </div>
     </form>
     <?php } ?>
 
-    <h2>ログイン</h2>
-    <form method="POST" action="/login/sign_in">
+    <h2 class="title">ログイン</h2>
+    <form ref="loginForm" method="POST" action="/login/sign_in">
         @csrf
         <div>
-            ID : <input type="text" name="id">
+            ID : <input type="text" v-model="id" name="id">
         </div>
         <div>
-            PW : <input type="text" name="password">
+            PW : <input type="text" v-model="password" name="password">
         </div>
         <div>
-            <input type="submit" value="送信">
+            <button type="button" v-on:click="loginSubmit">送信</button>
         </div>
     </form>
+    </main>
+    <script src="/js/build/login/index.js"></script>
 </body>
 
 </html>
